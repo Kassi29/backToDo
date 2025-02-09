9112734fbbdc74d1e1999067c12862236b5a3ade
Kass Badani <username@users.noreply.github.com>
2025-02-08 21:40:29 -0400

    Refactor TaskService: Replace EntityNotFoundException with custom NotFoundException and improve method names

--- src/main/java/com/kass/todo/services/TaskService.java
+++ src/main/java/com/kass/todo/services/TaskService.java
@@ -1,10 +1,10 @@
 package com.kass.todo.services;
 
+import com.kass.todo.exceptions.NotFoundException;
 import com.kass.todo.models.CategoryModel;
 import com.kass.todo.models.StatusModel;
 import com.kass.todo.models.TaskModel;
 import com.kass.todo.repositories.ITask;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,61 +15,62 @@ public class TaskService {
     private final ITask iTask;
 
 
-    private TaskModel getExistingTask(int id){
-        return iTask.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Task not found"));
-    }
-
     public TaskService(ITask iTask){
         this.iTask = iTask;
     }
 
     @Transactional(readOnly = true)
-    public List<TaskModel> getAll(){
+    public List<TaskModel> getAllTask(){
         return iTask.findAll();
     }
 
     @Transactional(readOnly = true)
-    public TaskModel getById(int id){
-        return getExistingTask(id);
+    public TaskModel getTaskById(int id){
+        return iTask.findById(id)
+                .orElseThrow(() -> new NotFoundException("Task not found"));
     }
 
     @Transactional
-    public TaskModel create(TaskModel taskModel){
+    public TaskModel createTask(TaskModel taskModel){
         return iTask.save(taskModel);
     }
 
     @Transactional
-    public TaskModel update (int id ,TaskModel taskModel){
-        TaskModel existingTask = getExistingTask(id);
+    public TaskModel updateTask(int id , TaskModel taskModel){
+        TaskModel existingTask = getTask(id);
 
-        existingTask.setCategory(taskModel.getCategory());
         existingTask.setDescription(taskModel.getDescription());
         existingTask.setName(taskModel.getName());
-            return iTask.save(existingTask);
+        return iTask.save(existingTask);
     }
 
     @Transactional
     public TaskModel updateStatus(int id, StatusModel statusModel){
-        TaskModel existingTask = getExistingTask(id);
+        TaskModel existingTask = getTask(id);
 
         existingTask.setStatus(statusModel);
         return iTask.save(existingTask);
-
     }
 
     @Transactional
-    public TaskModel updateCategory(int id, CategoryModel categoryModel){
-        TaskModel existingTask = getExistingTask(id);
+    public TaskModel  updateCategory(int id, CategoryModel categoryModel){
+        TaskModel existingTask = getTask(id);
 
         existingTask.setCategory(categoryModel);
         return iTask.save(existingTask);
-
     }
 
     @Transactional
-    public void delete(int id){
-        TaskModel existingTask = getExistingTask(id);
+    public void deleteTask(int id){
+        TaskModel existingTask = getTask(id);
         iTask.delete(existingTask);
     }
+
+    private TaskModel getTask(int id){
+        return iTask.findById(id)
+                .orElseThrow(() -> new NotFoundException("Task not found"));
+    }
+
+
+
 }
