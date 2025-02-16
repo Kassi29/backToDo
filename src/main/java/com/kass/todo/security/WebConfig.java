7c67b5c464cfcf32d0d3cdff2d278b17356ea190
Kass Badani <username@users.noreply.github.com>
2025-02-16 19:25:24 -0400

    Add allowed HTTP methods in CORS configuration

--- src/main/java/com/kass/todo/security/WebConfig.java
+++ src/main/java/com/kass/todo/security/WebConfig.java
@@ -1,10 +1,6 @@
 package com.kass.todo.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
 }
